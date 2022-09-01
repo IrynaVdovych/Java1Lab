682e6e94c8ed091662ecead08f9197bb6d176c2b
Iryna Vdpvych <Vdovych.Iryna@gmail.com>
2022-09-01 21:19:32 +0300

    First version

---
+++ src/com/example/lab1/Lab1.java
@@ -0,0 +1,30 @@
+package com.example.lab1;
+
+import java.util.Scanner;
+
+/**
+ * main class of the package
+ */
+public class Lab1 {
+    /**
+     * @param args array of parameters
+     */
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        LucasNumbers LN = new LucasNumbers();
+        System.out.println("1. Test 1");
+        System.out.print("Вкажіть номер числа Люка: ");
+        int n = sc.nextInt();
+        System.out.print(String.valueOf(n) + " число Люка: ");
+        System.out.println(LN.getLucasNumber(n));
+        System.out.println("\n2. Test 2");
+        System.out.print("Вкажіть кількість чисел Люка для тесту: ");
+        n = sc.nextInt();
+        System.out.println("Числа Люка серед перших " + String.valueOf(n) + ", які є кубами:");
+        int cubes[] = LN.getLucasCubeNumbers(n);
+        for(int k = 0; k < cubes.length; k++) {
+            System.out.print(" ");
+            System.out.println(cubes[k]);
+        }
+    }
+}
---
+++ src/com/example/lab1/LucasNumbers.java
@@ -0,0 +1,68 @@
+package com.example.lab1;
+
+import java.lang.Math;
+
+/**
+ * class for operate with Lucas numbers
+ */
+public class LucasNumbers {
+    private int l0 = 2;
+    private int l1 = 1;
+
+    /**
+     * this is a constructor
+     */
+    //конструктор
+    public LucasNumbers() {
+    }
+
+    /**
+     * @param n number of sequence(start from 0)
+     * @return value of n-th Lucas numbers
+     */
+    //метод повертає значення n-того числа Люка
+    public int getLucasNumber(int n) {
+        if (n < 0) return 0;
+        int lcur = l1 + l0;
+        int lprev = l1;
+        int lpprev = l0;
+        for (int k = 0; k < n; k++) {
+            lpprev = lprev;
+            lprev = lcur;
+            lcur = lprev + lpprev;
+        }
+        return lpprev;
+    }
+
+    /**
+     * @param n number
+     * @return is it a cube
+     */
+    private boolean testCube(int n) {
+        double r = Math.pow(n, 1 / 3);
+        int ir = (int) Math.round(r);
+        if (n == ir*ir*ir) return true;
+        else return false;
+    }
+
+    /**
+     * @param n number of sequence(start from 0)
+     * @return array of cubes among lucas numbers
+     */
+    public int[] getLucasCubeNumbers(int n) {
+        int tmpArr[] = new int[n + 1];
+        int nmbCubes = 0;
+        for (int k = 0; k <= n; k++) {
+            int lucas = getLucasNumber(k);
+            if (testCube(lucas)) {
+                tmpArr[nmbCubes] = lucas;
+                nmbCubes++;
+            }
+        }
+
+        int cubeArr[] = new int[nmbCubes];
+        for(int k = 0; k < nmbCubes; k++)
+            cubeArr[k] = tmpArr[k];
+        return cubeArr;
+    }
+}
