d1e21c9bd6c0c0109a07c59480dd30219cbbb172
IrynaVdovych <vdovych.iryna@gmail.com>
2022-09-08 21:41:05 +0300

    comments added

--- src/com/example/lab1/Lab1.java
+++ src/com/example/lab1/Lab1.java
@@ -1,5 +1,7 @@
+//оголошення пакету
 package com.example.lab1;
 
+//імпорт обʼєкта scanner для зчитування тексту з консолі
 import java.util.Scanner;
 
 /**
@@ -10,18 +12,27 @@ public class Lab1 {
      * @param args array of string parameters
      */
     public static void main(String args[]) {
+        //створюємо екземпляр класу scanner
         Scanner sc = new Scanner(System.in);
+        //створюємо екземпляр класу LucasNumbers
         LucasNumbers LN = new LucasNumbers();
         System.out.println("1. Test 1");
         System.out.print("Вкажіть номер числа Люка: ");
+        //зчитуємо ціле число з консолі у змінну n
         int n = sc.nextInt();
         System.out.print(String.valueOf(n) + " число Люка: ");
+        //за допомогою методу getLucasNumber визначаємо n-те число Люка і виводимо в консоль
         System.out.println(LN.getLucasNumber(n));
         System.out.println("\n2. Test 2");
         System.out.print("Вкажіть кількість чисел Люка для тесту: ");
+        //зчитуємо ціле число з консолі у змінну n
         n = sc.nextInt();
         System.out.println("Числа Люка серед перших " + String.valueOf(n) + ", які є кубами:");
+        //у масив цілих чисел cubes за допомогою методу get lucas cube numbers записуємо масив чисел люка,
+        // які є точними кубами
         int cubes[] = LN.getLucasCubeNumbers(n);
+
+        //ітеруємось індексом k по елементах масива cubes і виводимо на екран
         for(int k = 0; k < cubes.length; k++) {
             System.out.print(" ");
             System.out.println(cubes[k]);
--- src/com/example/lab1/LucasNumbers.java
+++ src/com/example/lab1/LucasNumbers.java
@@ -1,11 +1,14 @@
+//оголошення пакету
 package com.example.lab1;
 
+//імпорт математичної бібліотеки
 import java.lang.Math;
 
 /**
  * class for operate with Lucas numbers
  */
 public class LucasNumbers {
+    //приватні поля, які містять початкові значення послідовності чисел Люка
     private int l0 = 2;
     private int l1 = 1;
 
@@ -22,25 +25,33 @@ public class LucasNumbers {
      */
     //метод повертає значення n-того числа Люка
     public int getLucasNumber(int n) {
+        //захист від некоректного значення
         if (n < 0) return 0;
-        int lcur = l1 + l0;
-        int lprev = l1;
-        int lpprev = l0;
+        //змінні для обчислення трьох послідовних чисел Люка
+        int lcur = l1 + l0; //друге число
+        int lprev = l1; //перше число
+        int lpprev = l0;  //нульове число
+        //цикл обчислення наступних членів послідовності чисел люка
         for (int k = 0; k < n; k++) {
-            lpprev = lprev;
-            lprev = lcur;
-            lcur = lprev + lpprev;
+            lpprev = lprev; //к-те число
+            lprev = lcur; //к+1 число
+            lcur = lprev + lpprev; //к+2 число
         }
-        return lpprev;
+        return lpprev; //повертаємо n-те число люка
     }
 
     /**
      * @param n number
      * @return is it a cube
      */
+
+    //привтаний метод пеервіряє чи є число параметр кубом якогось цілого числа
     private boolean testCube(int n) {
+        //корінь третьої степені із числа n
         double r = Math.pow(n, 1. / 3);
+        //заокруглюємо результат до найближчого цілого числа
         int ir = (int) Math.round(r);
+        //якщо куб цього числа є числом n, то повертаємо істину, інакше хибність
         if (n == ir*ir*ir) return true;
         else return false;
     }
@@ -49,19 +60,31 @@ public class LucasNumbers {
      * @param n number of sequence(start from 0)
      * @return array of cubes among first n lucas numbers
      */
+
+    //повертає масив перших n чисел люка, які є точними кубами
     public int[] getLucasCubeNumbers(int n) {
+        //масив цілих чисел для збереження результатів пошуку
         int tmpArr[] = new int[n + 1];
+        //лічильник знайдених кубів
         int nmbCubes = 0;
+        //цикл по індексах від 0 до n включно
         for (int k = 0; k <= n; k++) {
+            //обчислюємо k-те число Люка
             int lucas = getLucasNumber(k);
+            //якщо воно є кубом
             if (testCube(lucas)) {
+                //додаємо його до масиву
                 tmpArr[nmbCubes] = lucas;
+                //збільшуємо лічильник
                 nmbCubes++;
             }
         }
 
+        //масив цілих чисел в якому зберігаємо результат пошуку(без зайвих нулів)
         int cubeArr[] = new int[nmbCubes];
+        //ітеруємося по знайдених nmbcubes перших елементах масиву tmparr
         for(int k = 0; k < nmbCubes; k++)
+            //копіюємо значення масиву tmparr в масив cubearr
             cubeArr[k] = tmpArr[k];
         return cubeArr;
     }
